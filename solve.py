85abca7fe39d453d5435c21e1a1c0af2eda8770b
Runar Borge <runar@borge.nu>
2021-04-06 10:22:12 +0200

    Update formatting and indentations

--- solve.py
+++ solve.py
@@ -4,11 +4,14 @@ from enum import Enum
 from collections import defaultdict
 from pprint import pprint
 import time
+
+
 class DIR(Enum):
     UP = 'U',
     DOWN = 'D',
     LEFT = 'L',
     RIGHT = 'R',
+
 
 class NoWrapList(list):
     def __getitem__(self, item):
@@ -30,8 +33,10 @@ init_board = NoWrapList([
               NoWrapList([ ' ', ' ', ' ', 'I', 'I', 'I', ' ', ' ', ' ']),
              ])
 
+
 def count(board):
     return sum([len([y for y in x if y == 'I']) for x in board])
+
 
 def check_isolated(board, first_only=False):
     isolated = []
@@ -74,29 +79,30 @@ def check_isolated(board, first_only=False):
             if y < 8 and x < 8 and board[y+1][x+1] == 'I':
                 continue
             if first_only:
-                return [(y,x)]
-            isolated.append((y,x))
+                return [(y, x)]
+            isolated.append((y, x))
     return isolated
 
 
 def yield_moveable(board):
-  for y in range(9):
-    for x in range(9):
-        # Check for moveable object on position
-        if board[y][x] != 'I':
-            continue
-        # Test Left
-        if x > 1 and board[y][x-1] == 'I' and board[y][x-2] == 'O':
+    for y in range(9):
+        for x in range(9):
+            # Check for moveable object on position
+            if board[y][x] != 'I':
+                continue
+            # Test Left
+            if x > 1 and board[y][x-1] == 'I' and board[y][x-2] == 'O':
                 yield y, x, DIR.LEFT
-        # Test Right
-        if x < 7 and board[y][x+1] == 'I' and board[y][x+2] == 'O':
+            # Test Right
+            if x < 7 and board[y][x+1] == 'I' and board[y][x+2] == 'O':
                 yield y, x, DIR.RIGHT
-        # Test Up
-        if y > 1 and board[y-1][x] == 'I' and board[y-2][x] == 'O':
+            # Test Up
+            if y > 1 and board[y-1][x] == 'I' and board[y-2][x] == 'O':
                 yield y, x, DIR.UP
-        # Test Down
-        if y < 7 and board[y+1][x] == 'I' and board[y+2][x] == 'O':
+            # Test Down
+            if y < 7 and board[y+1][x] == 'I' and board[y+2][x] == 'O':
                 yield y, x, DIR.DOWN
+
 
 def moveable(board, y, x, dir):
     # Test if this position is movable
@@ -123,9 +129,9 @@ def move(board, y, x, dir):
     try:
         if dir == DIR.LEFT:
             if not board[y][x-1] == 'I':
-                raise Exception('Unable to move y%sx%s in direction %s, Nothing to jump over' % (y,x,dir))
+                raise Exception('Unable to move y%sx%s in direction %s, Nothing to jump over' % (y, x, dir))
             if not board[y][x-2] == 'O':
-                raise Exception('Unable to move y%sx%s in direction %s, Target is occupied' % (y,x,dir))
+                raise Exception('Unable to move y%sx%s in direction %s, Target is occupied' % (y, x, dir))
 
             board[y][x-2] = 'I'
             board[y][x-1] = 'O'
@@ -133,34 +139,34 @@ def move(board, y, x, dir):
 
         elif dir == DIR.RIGHT:
             if not board[y][x+1] == 'I':
-                raise Exception('Unable to move y%sx%s in direction %s, Nothing to jump over' % (y,x,dir))
+                raise Exception('Unable to move y%sx%s in direction %s, Nothing to jump over' % (y, x, dir))
             if not board[y][x+2] == 'O':
-                raise Exception('Unable to move y%sx%s in direction %s, Target is occupied' % (y,x,dir))
+                raise Exception('Unable to move y%sx%s in direction %s, Target is occupied' % (y, x, dir))
             board[y][x+2] = 'I'
             board[y][x+1] = 'O'
-            board[y][x+0] = 'O' 
+            board[y][x+0] = 'O'
 
         elif dir == DIR.UP:
             if not board[y-1][x] == 'I':
-                raise Exception('Unable to move y%sx%s in direction %s, Nothing to jump over' % (y,x,dir))
+                raise Exception('Unable to move y%sx%s in direction %s, Nothing to jump over' % (y, x, dir))
             if not board[y-2][x] == 'O':
-                raise Exception('Unable to move y%sx%s in direction %s, Target is occupied' % (y,x,dir))
+                raise Exception('Unable to move y%sx%s in direction %s, Target is occupied' % (y, x, dir))
             board[y-2][x] = 'I'
             board[y-1][x] = 'O'
             board[y-0][x] = 'O'
 
         elif dir == DIR.DOWN:
             if not board[y+1][x] == 'I':
-                raise Exception('Unable to move y%sx%s in direction %s, Nothing to jump over' % (y,x,dir))
+                raise Exception('Unable to move y%sx%s in direction %s, Nothing to jump over' % (y, x, dir))
             if not board[y+2][x] == 'O':
-                raise Exception('Unable to move y%sx%s in direction %s, Target is occupied' % (y,x,dir))
+                raise Exception('Unable to move y%sx%s in direction %s, Target is occupied' % (y, x, dir))
             board[y+2][x] = 'I'
             board[y+1][x] = 'O'
             board[y+0][x] = 'O'
         else:
-            raise Exception('Unable to move y%sx%s in direction %s, Unknown direction' % (y,x,repr(dir)))
+            raise Exception('Unable to move y%sx%s in direction %s, Unknown direction' % (y, x, repr(dir)))
     except IndexError:
-        raise Exception('Unable to move y%sx%s in direction %s, To near the edge' % (y,x,dir))
+        raise Exception('Unable to move y%sx%s in direction %s, To near the edge' % (y, x, dir))
 
 
 def move2(board, y, x, dir):
@@ -215,36 +221,35 @@ def undo(board, y, x, dir):
     try:
         if dir == DIR.LEFT:
             if not (board[y][x-2] == 'I' and board[y][x-1] == 'O' and board[y][x-0] == 'O'):
-                raise Exception('Unable to undo y%sx%s in direction %s, not a valid move' % (y,x,dir))
+                raise Exception('Unable to undo y%sx%s in direction %s, not a valid move' % (y, x, dir))
             board[y][x-2] = 'O'
             board[y][x-1] = 'I'
             board[y][x-0] = 'I'
-        
+
         elif dir == DIR.RIGHT:
             if not (board[y][x+2] == 'I' and board[y][x+1] == 'O' and board[y][x+0] == 'O'):
-                raise Exception('Unable to undo y%sx%s in direction %s, not a valid move' % (y,x,dir))
+                raise Exception('Unable to undo y%sx%s in direction %s, not a valid move' % (y, x, dir))
             board[y][x+2] = 'O'
             board[y][x+1] = 'I'
-            board[y][x+0] = 'I' 
+            board[y][x+0] = 'I'
 
         elif dir == DIR.UP:
             if not (board[y-2][x] == 'I' and board[y-1][x] == 'O' and board[y-0][x] == 'O'):
-                raise Exception('Unable to undo y%sx%s in direction %s, not a valid move' % (y,x,dir))
+                raise Exception('Unable to undo y%sx%s in direction %s, not a valid move' % (y, x, dir))
             board[y-2][x] = 'O'
             board[y-1][x] = 'I'
             board[y-0][x] = 'I'
 
         elif dir == DIR.DOWN:
             if not (board[y+2][x] == 'I' and board[y+1][x] == 'O' and board[y+0][x] == 'O'):
-                raise Exception('Unable to undo y%sx%s in direction %s, not a valid move' % (y,x,dir))
+                raise Exception('Unable to undo y%sx%s in direction %s, not a valid move' % (y, x, dir))
             board[y+2][x] = 'O'
             board[y+1][x] = 'I'
             board[y+0][x] = 'I'
         else:
-            raise Exception('Unable to undo y%sx%s in direction %s, Unknown direction' % (y,x,repr(dir)))
+            raise Exception('Unable to undo y%sx%s in direction %s, Unknown direction' % (y, x, repr(dir)))
     except IndexError:
-        raise Exception('Unable to undo y%sx%s in direction %s, To near the edge' % (y,x,dir))
-
+        raise Exception('Unable to undo y%sx%s in direction %s, To near the edge' % (y, x, dir))
 def undo2(board, y, x, dir):
     if dir == DIR.LEFT:
         if x <= 1:
@@ -292,12 +297,15 @@ def print_board(board):
         print(i, " ".join(x))
     print("Y")
 
+
 nr = 0
 result = []
 tries = 0
 le = defaultdict(int)
 last_time = time.time()
-def solve(board,stack=0):
+
+
+def solve(board, stack=0):
     global tries, result, nr, le, last_time
     tries += 1
 
@@ -327,16 +335,15 @@ def solve(board,stack=0):
     return False
 
 
-
 def main():
     print('Initial Board Layout')
     print_board(init_board)
-    #for x in yield_moveable(init_board):
-    #    print(f'Move y{x[0]}x{x[1]} {x[2]!r}')
-    #    move(init_board, *x)
-    #    print_board(init_board)
-    #    undo(init_board, *x)
-    #    print_board(init_board)
+    # for x in yield_moveable(init_board):
+    #     print(f'Move y{x[0]}x{x[1]} {x[2]!r}')
+    #     move(init_board, *x)
+    #     print_board(init_board)
+    #     undo(init_board, *x)
+    #     print_board(init_board)
     start = time.time()
     solve(init_board)
     end = time.time()
@@ -344,16 +351,15 @@ def main():
     print("type delta", type(delta))
     print(end - start)
 
-    #for i, s in enumerate(result):
-    #    print('%04d   %s' % (i , s))
-    #print('Current Board')
-    #print_board(init_board)
-    print("Number",nr)
+    # for i, s in enumerate(result):
+    #     print('%04d   %s' % (i , s))
+    # print('Current Board')
+    # print_board(init_board)
+    print("Number", nr)
 
-
-    #for i in yield_moveable(init_board):
-    #    print(i)
-    #return
+    # for i in yield_moveable(init_board):
+    #     print(i)
+    # return
 
 
 if __name__ == "__main__":
